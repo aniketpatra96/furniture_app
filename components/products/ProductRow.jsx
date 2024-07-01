251c7cad64d2da810884fe78ead375596567a479
Bishal011 <bishalpradhan061@gmail.com>
2024-07-01 19:50:45 +0530

    infinite scroll updated

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -89,36 +89,126 @@
 // export default ProductRow;
 
 
+// import React, { useState, useEffect } from "react";
+// import { View, FlatList, ActivityIndicator, Text, TouchableOpacity } from "react-native";
+// import { COLORS } from "../../constants";
+// import ProductCardView from "./ProductCardView";
+// import useFetch from "../../hook/useFetch";
+// import styles from "./productRow.style";
+
+// const ProductRow = () => {
+//   const { data, isLoading, error, refetch } = useFetch(); // Using custom hook to fetch data
+//   const [currentPage, setCurrentPage] = useState(1); // State to track current page number
+//   const itemsPerPage = 5; // Number of items per page
+
+//   useEffect(() => {
+//     refetch(); // Fetch data initially
+//   }, []);
+
+//   const totalPages = Math.ceil(data.length / itemsPerPage); // Calculate total pages based on data length
+
+//   const paginatedData = data.slice(
+//     (currentPage - 1) * itemsPerPage,
+//     currentPage * itemsPerPage
+//   ); // Slice data based on current page and items per page
+
+//   const handleNextPage = () => {
+//     setCurrentPage((prevPage) => prevPage + 1); // Function to increment current page
+//   };
+
+//   const handlePrevPage = () => {
+//     setCurrentPage((prevPage) => prevPage - 1); // Function to decrement current page
+//   };
+
+//   if (isLoading) {
+//     return (
+//       <View style={styles.loadingContainer}>
+//         <ActivityIndicator size="large" color={COLORS.primary} />
+//       </View>
+//     );
+//   }
+
+//   if (error) {
+//     return (
+//       <View style={styles.errorContainer}>
+//         <Text>Something went wrong!!</Text>
+//       </View>
+//     );
+//   }
+
+//   return (
+//     <View>
+//       <FlatList
+//         data={paginatedData}
+//         keyExtractor={(item) => item._id}
+//         renderItem={({ item }) => (
+//           <View style={styles.card}>
+//             <ProductCardView item={item} />
+//           </View>
+//         )}
+//         numColumns={1} // Ensure one card per row
+//         contentContainerStyle={styles.flatListContent}
+//       />
+//       <View style={styles.paginationContainer}>
+//         <Text style={styles.paginationText}>
+//           Page {currentPage} of {totalPages}
+//         </Text>
+//         <View style={styles.paginationButtons}>
+//           <TouchableOpacity
+//             style={[styles.paginationButton, currentPage === 1 && styles.disabled]}
+//             onPress={handlePrevPage}
+//             disabled={currentPage === 1}
+//           >
+//             <Text style={{ color: COLORS.primary }}>Previous</Text>
+//           </TouchableOpacity>
+//           <TouchableOpacity
+//             style={[
+//               styles.paginationButton,
+//               currentPage === totalPages && styles.disabled,
+//             ]}
+//             onPress={handleNextPage}
+//             disabled={currentPage === totalPages}
+//           >
+//             <Text style={{ color: COLORS.primary }}>Next</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default ProductRow;
+
+
+
 import React, { useState, useEffect } from "react";
-import { View, FlatList, ActivityIndicator, Text, TouchableOpacity } from "react-native";
+import { View, FlatList, ActivityIndicator, Text } from "react-native";
 import { COLORS } from "../../constants";
 import ProductCardView from "./ProductCardView";
 import useFetch from "../../hook/useFetch";
 import styles from "./productRow.style";
 
 const ProductRow = () => {
-  const { data, isLoading, error, refetch } = useFetch(); // Using custom hook to fetch data
-  const [currentPage, setCurrentPage] = useState(1); // State to track current page number
-  const itemsPerPage = 5; // Number of items per page
+  const { data, isLoading, error, refetch } = useFetch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const itemsPerPage = 5;
 
   useEffect(() => {
-    refetch(); // Fetch data initially
+    refetch();
   }, []);
 
-  const totalPages = Math.ceil(data.length / itemsPerPage); // Calculate total pages based on data length
-
-  const paginatedData = data.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
-  ); // Slice data based on current page and items per page
-
-  const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1); // Function to increment current page
+  const handleLoadMore = () => {
+    if (currentPage * itemsPerPage < data.length) {
+      setIsLoadingMore(true);
+      setTimeout(() => {
+        setCurrentPage((prevPage) => prevPage + 1);
+        setIsLoadingMore(false);
+      }, 1000);
+    }
   };
 
-  const handlePrevPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1); // Function to decrement current page
-  };
+  const paginatedData = data.slice(0, currentPage * itemsPerPage);
 
   if (isLoading) {
     return (
@@ -137,43 +227,25 @@ const ProductRow = () => {
   }
 
   return (
-    <View>
-      <FlatList
-        data={paginatedData}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <ProductCardView item={item} />
-          </View>
-        )}
-        numColumns={1} // Ensure one card per row
-        contentContainerStyle={styles.flatListContent}
-      />
-      <View style={styles.paginationContainer}>
-        <Text style={styles.paginationText}>
-          Page {currentPage} of {totalPages}
-        </Text>
-        <View style={styles.paginationButtons}>
-          <TouchableOpacity
-            style={[styles.paginationButton, currentPage === 1 && styles.disabled]}
-            onPress={handlePrevPage}
-            disabled={currentPage === 1}
-          >
-            <Text style={{ color: COLORS.primary }}>Previous</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[
-              styles.paginationButton,
-              currentPage === totalPages && styles.disabled,
-            ]}
-            onPress={handleNextPage}
-            disabled={currentPage === totalPages}
-          >
-            <Text style={{ color: COLORS.primary }}>Next</Text>
-          </TouchableOpacity>
+    <FlatList
+      data={paginatedData}
+      keyExtractor={(item) => item._id}
+      renderItem={({ item }) => (
+        <View style={styles.card}>
+          <ProductCardView item={item} />
         </View>
-      </View>
-    </View>
+      )}
+      contentContainerStyle={styles.flatListContent}
+      onEndReached={handleLoadMore}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={
+        isLoadingMore && (
+          <View style={styles.infiniteScrollSpinner}>
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          </View>
+        )
+      }
+    />
   );
 };
 
--- components/products/productRow.style.js
+++ components/products/productRow.style.js
@@ -5,54 +5,31 @@ const styles = StyleSheet.create({
   flatListContent: {
     paddingHorizontal: SIZES.medium,
     paddingBottom: SIZES.large,
+    marginBottom: 100
   },
   loadingContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginBottom: 100
   },
   errorContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginBottom: 100
   },
   card: {
     flex: 1,
     marginVertical: SIZES.small,
+    marginBottom: 20
   },
-
-
-
-  paginationContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  infiniteScrollSpinner: {
+    marginVertical: SIZES.medium,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 10,
-    paddingHorizontal: 20,
-    marginBottom: 120,
-  },
-  paginationText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: COLORS.primary,
-  },
-  paginationButtons: {
-    flexDirection: 'row',
-  },
-  paginationButton: {
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginHorizontal: 5,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: COLORS.primary,
-    color: COLORS.primary,
-  },
-  disabled: {
-    opacity: 0.5, // Example of disabled button style
-  },
-
-
+    marginBottom: 100
+  }
 });
 
 export default styles;
